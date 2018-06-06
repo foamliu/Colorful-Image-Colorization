a126abef34af69613cca0f25270cebcf64b5c12e
Yang Liu <foamliu@yeah.net>
2018-06-06 20:57:11 +0800

    update

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-import scipy.io
-import numpy as np
-
 img_rows, img_cols = 256, 256
 channel = 3
 batch_size = 96
@@ -11,9 +8,6 @@ num_valid_samples = 5000
 num_classes = 313
 epsilon = 1e-6
 epsilon_sqr = epsilon ** 2
-
-mat = scipy.io.loadmat('human_colormap.mat')
-color_map = (mat['colormap'] * 256).astype(np.int32)
 
 nb_neighbors = 5
 # temperature parameter T
