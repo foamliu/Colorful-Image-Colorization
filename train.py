680fb99ba18eebd4734c9b23f87886fa597b84da
Yang Liu <foamliu@yeah.net>
2018-07-05 14:01:47 +0800

    update

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         if pretrained_path is not None:
             new_model.load_weights(pretrained_path)
 
-    sgd = keras.optimizers.SGD(lr=1e-5, decay=1e-6, momentum=0.9, nesterov=True)
+    sgd = keras.optimizers.SGD(lr=3e-6, decay=1e-6, momentum=0.9, nesterov=True)
     # new_model.compile(optimizer='nadam', loss='categorical_crossentropy')
     new_model.compile(optimizer=sgd, loss=categorical_crossentropy_color)
 
